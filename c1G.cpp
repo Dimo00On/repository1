1c307912ffd9eeefb459fc3dc9c9b31ce779c983
Dimon <konkov.da@phystech.edu>
2021-10-24 18:41:24 +0300

    rewrite G task

--- c1G.cpp
+++ c1G.cpp
@@ -1,29 +1,37 @@
-    #include <iostream>
-    #include <vector>
-     
-     
-    int main()
-    {
-        long long n,p,q,maxa;
-        maxa=0;
-        std::cin >> n >> p >> q;
-        std::vector <long long> a(n);
-        for (int i=0;i<n;++i) {
-            std::cin >> a[i];
-            if (a[i]>maxa) {maxa=a[i];}
+#include <iostream>
+#include <vector>
+
+const long long MaxMonsterHP = 1e9;
+void pleaseWinThisGame() {
+    long long monsterAmount;
+    long long mainDamage;
+    long long splashDamage;
+    long long bossHP = 0;
+    std::cin >> monsterAmount >> mainDamage >> splashDamage;
+    std::vector <long long> hp(monsterAmount);
+    for (int i = 0; i < monsterAmount; ++i) {
+        std::cin >> hp[i];
+        if (hp[i] > bossHP) {
+            bossHP = hp[i];
         }
-        if (p==q) {std::cout << (maxa+p-1)/q; return 0;}
-        long long l=0;
-        long long r=1e9;
-        while (r-l>1) {
-            long long m=(r+l)/2;
-            long long kolyd=0;
-            for (int i=0;i<n;++i) {
-                kolyd=kolyd+(std::max((a[i]-m*q),(long long)0)+(p-q)-1)/(p-q);
-            }
-            if (kolyd>m) {l=m;}
-            else {r=m;}
-        }
-        std::cout << r;
-        return 0;
     }
+    if (mainDamage == splashDamage) {
+        std::cout << (bossHP + mainDamage - 1) / splashDamage;
+        return;
+    }
+    long long left = 0;
+    long long right = MaxMonsterHP;
+    while (right - left > 1) {
+        long long middle = (right + left) / 2;
+        long long hitAmount = 0;
+        long long zero = 0;
+        for (int i = 0; i < monsterAmount; ++i) {
+            hitAmount += (std::max((hp[i] - middle * splashDamage), zero) + mainDamage - splashDamage - 1) / (mainDamage - splashDamage);
+        }
+        hitAmount > middle ? left = middle : right = middle;
+    }
+    std::cout << right;
+}
+int main(){
+    pleaseWinThisGame();
+}
