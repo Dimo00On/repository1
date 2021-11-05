e473e4db0374b3cec84b6e60b4af84db9195c929
Dimon <konkov.da@phystech.edu>
2021-11-05 17:52:38 +0300

    rewrite B task

--- c2B.cpp
+++ c2B.cpp
@@ -1,101 +1,85 @@
-    #include <iostream>
-    #include <vector>
-     
-    struct border1{
-        int val[10];
-        bool type;
-    };
-    struct border2{
-        int val;
-        bool type;
-    };
-    namespace N {
-        border1 x;
-        border1 ans[20000];
-        border1 numb[20000];
+#include <iostream>
+#include <vector>
+
+const int MaxLength = 10;
+const int NumberSystem = 10;
+struct border{ // для хранения границы отрезка. type означает, начало или конец отрезка.
+    // value означает координаты соответствующей границы отрезка. partValue означает разбиение числа value по цифрам
+    std::vector<int> partValue;
+    bool type = false;
+    int value = 0;
+};
+void stabsort(int size, int step, std::vector<border>& answer, std::vector<border>& number) {
+    if (step == MaxLength) {
+        return;
     }
-    void stabsort(int n, int step) {
-      if (step != 10) {
-        using namespace N;
-        if (step>0) {
-          for (int i = 0; i < n; ++i) {
-            numb[i] = ans[i];
-          }
+    if (step) {
+        for (int i = 0; i < size; ++i) {
+            number[i] = answer[i];
         }
-        int cnt[10];
-        for (int i = 0; i < 10; ++i) {
-            cnt[i] = 0;
-        }
-        for (int i = 0; i < n; ++i) {
-          ++cnt[numb[i].val[step]];
-        }
-        for (int i = 1; i <= 9; ++i) {
-          cnt[i] += cnt[i-1];
-        }
-        for (int i = n - 1; i >= 0; --i) {
-          ans[--cnt[numb[i].val[step]]]=numb[i];
-        }
-        stabsort(n, step + 1);
-      }
     }
-    int main()
-    {
-      using namespace N;
-      for (int i = 0; i < 10; ++i) {
-        x.val[i] = 0;
-      }
-      x.type = 0;
-      for (int i = 0; i <= 10; ++i) {
-        numb[i] = x ;
-        ans[i] = x;
-      }
-      int n;
-      std::cin >> n;
-      for (int i = 0; i < n; ++i) {
-        int l;
-        std::cin >> l;
-        int p=0;
-        while (l>0) {
-          numb[i].val[p] = l%10;
-          l /= 10;
-          ++p;
+    std::vector<int> count(NumberSystem, 0);
+    for (int i = 0; i < size; ++i) {
+        ++count[number[i].partValue[step]];
+    }
+    for (int i = 1; i < NumberSystem; ++i) {
+        count[i] += count[i - 1];
+    }
+    for (int i = size - 1; i >= 0; --i) {
+        answer[--count[number[i].partValue[step]]] = number[i];
+    }
+    stabsort(size, step + 1, answer, number);
+}
+void pleaseSolveThisTask(const std::string& uselessString) {
+    border edgeZero;
+    edgeZero.partValue.resize(MaxLength);
+    for (int i = 0; i < MaxLength; ++i) {
+        edgeZero.partValue[i] = 0;
+    }
+    int segmentAmount;
+    std::cin >> segmentAmount;
+    std::vector<border> answer(2 * segmentAmount);
+    std::vector<border> number(2 * segmentAmount);
+    for (int i = 0; i < 2 * segmentAmount; ++i) {
+        number[i] = edgeZero;
+        answer[i] = edgeZero;
+    }
+    for (int i = 0; i < segmentAmount; ++i) {
+        int left, right;
+        std::cin >> left >> right;
+        number[i].type = true;
+        number[i].value = left;
+        number[i + segmentAmount].value = right;
+        int length = 0;
+        while (left) {
+            number[i].partValue[length] = left % NumberSystem;
+            left /= NumberSystem;
+            ++length;
         }
-        numb[i].type = 1;
-        int r;
-        std::cin >> r;
-        p = 0;
-        while (r>0) {
-          numb[i+n].val[p] = r%10;
-          r /= 10;
-          ++p;
+        length = 0;
+        while (right) {
+            number[i + segmentAmount].partValue[length] = right % NumberSystem;
+            right /= NumberSystem;
+            ++length;
         }
-        numb[i+n].type = 0;
-      }
-      stabsort(2*n, 0);
-      border2 bord[20000];
-      for (int i = 0; i < 2*n; ++i) {
-        bord[i].val=0;
-      }
-      for (int i = 0; i < 2*n; ++i) {
-        for (int j = 9; j >= 0; --j) {
-            bord[i].val = bord[i].val * 10 + ans[i].val[j];
-        }
-        bord[i].type = ans[i].type;
-      }
-      int lay = 1;
-      int start = bord[0].val;
-      int sum = 0;
-      for (int i = 1; i < 2*n; ++i) {
+    }
+    stabsort(2 * segmentAmount, 0, answer, number);
+    int lay = 1;
+    int start = answer[0].value;
+    int sum = 0;
+    for (int i = 1; i < 2 * segmentAmount; ++i) {
         if (lay == 1) {
-          lay += (bord[i].type ? 1 : -1);
-          sum += (bord[i].val - start);
-          continue;
+            lay += (answer[i].type ? 1 : -1);
+            sum += (answer[i].value - start);
+            continue;
         }
-        lay += (bord[i].type ? 1 : -1);
+        lay += (answer[i].type ? 1 : -1);
         if (lay == 1) {
-            start = bord[i].val;
+            start = answer[i].value;
         }
-      }
-      std::cout << sum;
-      return 0;
     }
+    std::cout << sum;
+}
+int main(){
+    pleaseSolveThisTask("B task");
+}
