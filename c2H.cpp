83f597ac3c51f6c5e1114deafe210e80ba9ce727
Dimon <konkov.da@phystech.edu>
2021-11-11 16:43:49 +0300

    rewrite H task

--- c2H.cpp
+++ c2H.cpp
@@ -1,68 +1,61 @@
 #include <iostream>
 #include <vector>
- 
-const int partAmount = 8;
-const int eightByteNumber = 256;
- 
-struct big{
-    int part[partAmount];
+
+const int PartAmount = 8;
+const long long EightByteNumber = 256;
+
+struct BigInteger{
+    std::vector<int> part;
+    BigInteger(){
+        part.resize(PartAmount);
+    }
 };
-namespace BigNum{
-    int amount;
-    std::vector<big> numbers;
-}
-void LSD(int step) {\
-    using namespace BigNum;
-    std::vector<int> count(eightByteNumber);
-    for (int i = 0; i < eightByteNumber; ++i) {
-        count[i] = 0;
+void LSD(int step, std::vector<BigInteger>& numbers) {\
+    int numberAmount = static_cast<int>(numbers.size());
+    std::vector<int> count(EightByteNumber, 0);
+    for (int i = 0; i < numberAmount; ++i) {
+        ++count[numbers[i].part[step]];
     }
-    for (int i = 0; i < amount; ++i) {
-        count[numbers[i].part[step]]++;
-    }
-    for (int i = 1; i < eightByteNumber; ++i) {
+    for (long long i = 1; i < EightByteNumber; ++i) {
         count[i] += count[i - 1];
     }
-    std::vector<big> answer(amount);
-    for (int i = amount - 1; i >= 0; --i) {
-        count[numbers[i].part[step]]--;
+    std::vector<BigInteger> answer(numberAmount);
+    for (int i = numberAmount - 1; i >= 0; --i) {
+        --count[numbers[i].part[step]];
         answer[count[numbers[i].part[step]]] = numbers[i];
     }
-    for (int i = 0; i < amount; ++i) {
+    for (int i = 0; i < numberAmount; ++i) {
         numbers[i] = answer[i];
     }
-    count.clear();
-    answer.clear();
 }
-int main(){
-    using namespace BigNum;
+void pleaseSolveThisTask(const std::string& uselessString) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-    std::cin >> amount;
-    for (int i = 0; i < amount; ++i) {
-        long long bigNumb;
-        std::cin >> bigNumb;
-        big forPushing;
-        for (int j = 0; j < partAmount; ++j) {
-            forPushing.part[j] = 0;
-        }
-        numbers.push_back(forPushing);
+    int numberAmount;
+    std::cin >> numberAmount;
+    std::vector<BigInteger> numbers(numberAmount);
+    for (int i = 0; i < numberAmount; ++i) {
+        long long bigNumber;
+        std::cin >> bigNumber;
         int j = 0;
-        while (bigNumb > 0) {
-            numbers[i].part[j] = bigNumb % eightByteNumber;
+        while (bigNumber > 0) {
+            numbers[i].part[j] = static_cast<int>(bigNumber % EightByteNumber);
             ++j;
-            bigNumb /= eightByteNumber;
+            bigNumber /= EightByteNumber;
         }
     }
-    for (int i = 0; i < partAmount; ++i) {
-        LSD(i);
+    for (int i = 0; i < PartAmount; ++i) {
+        LSD(i, numbers);
     }
-    for (int i = 0; i < amount; ++i) {
-        long long bigNumb = 0;
-        for (int j = partAmount - 1; j >= 0; --j) {
-            bigNumb = bigNumb * eightByteNumber + numbers[i].part[j];
+    for (int i = 0; i < numberAmount; ++i) {
+        long long bigNumber = 0;
+        for (int j = PartAmount - 1; j >= 0; --j) {
+            bigNumber = bigNumber * EightByteNumber + numbers[i].part[j];
         }
-        std::cout << bigNumb << " ";
+        std::cout << bigNumber << " ";
     }
     std::cout << std::endl;
+}
+int main(){
+    pleaseSolveThisTask("H task");
 }
