90dcd077ddfb5d385f201cea20f88ed11b5322a7
Dimon <konkov.da@phystech.edu>
2021-11-11 16:04:46 +0300

    rewrite C task

--- c2C.cpp
+++ c2C.cpp
@@ -1,65 +1,60 @@
-    #include <iostream>
-    #include <vector>
-     
-    namespace N{
-        std::vector <int> a;
-        int less;
-        int equal;
-    }
-    void partition(int l, int r, int x){
-      using namespace N;
-      std::vector <int> b;
-      less = 0;
-      equal = 0;
-      for (int i = l; i < r; ++i) {
-        if (a[i] < x) {
-          b.push_back(a[i]);
-          ++less;
+#include <iostream>
+#include <vector>
+#include <string>
+
+const int RandFunctionRange = 32768;
+void partition(int left, int right, int pivot, int& less, int& equal, std::vector<int>& array){
+    std::vector <int> b;
+    for (int i = left; i < right; ++i) {
+        if (array[i] < pivot) {
+            b.push_back(array[i]);
+            ++less;
         }
-      }
-      for (int i = l; i < r; ++i) {
-        if (a[i] == x) {
-          b.push_back(a[i]);
-          ++equal;
+    }
+    for (int i = left; i < right; ++i) {
+        if (array[i] == pivot) {
+            b.push_back(array[i]);
+            ++equal;
         }
-      }
-      for (int i = l; i < r; ++i) {
-        if (a[i] > x) {
-          b.push_back(a[i]);
+    }
+    for (int i = left; i < right; ++i) {
+        if (array[i] > pivot) {
+            b.push_back(array[i]);
         }
-      }
-      for (int i = l, j = 0; i < r; ++i, ++j) {
-        a[i]=b[j];
-      }
-      b.clear();
     }
-     
-    int QuickSelect(int l, int r, int k) {
-      using namespace N;
-      if (r == l + 1) {
-        return a[l];
-      }
-      int ipiv=(rand()*32768+rand())%(r-l);
-      int piv = a[ipiv+l];
-      partition(l, r, piv);
-      if (k < less) {
-        return QuickSelect(l, l+less, k);
-      }
-      if (k < less + equal) {
-        return piv;
-      }
-      return QuickSelect(l+less+equal, r, k-less-equal);
+    for (int i = left, j = 0; i < right; ++i, ++j) {
+        array[i] = b[j];
     }
-    int main(){
-      using namespace N;
-      int n, k;
-      std::ios_base::sync_with_stdio(false);
-      std::cin.tie(NULL);
-      std::cin >> n >> k;
-      for (int i = 0; i < n; ++i) {
-        int x;
-        std::cin >> x;
-        a.push_back(x);
-      }
-      std::cout << QuickSelect(0, n, k);
+    b.clear();
+}
+int QuickSelect(int left, int right, int sortedIndex, std::vector<int>& array) {
+    if (right == left + 1) {
+        return array[left];
     }
+    int indexPivot = (rand() * RandFunctionRange + rand()) % (right - left);
+    int pivot = array[indexPivot + left];
+    int less = 0;
+    int equal = 0;
+    partition(left, right, pivot, less, equal, array);
+    if (sortedIndex < less) {
+        return QuickSelect(left, left + less, sortedIndex, array);
+    }
+    if (sortedIndex < less + equal) {
+        return pivot;
+    }
+    return QuickSelect(left + less + equal, right, sortedIndex - less - equal, array);
+}
+void pleaseSolveThisTask(const std::string& uselessString) {
+    int size, sortedIndex;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cin >> size >> sortedIndex;
+    std::vector<int> array(size);
+    for (int i = 0; i < size; ++i) {
+        std::cin >> array[i];
+    }
+    std::cout << QuickSelect(0, size, sortedIndex, array);
+}
+int main(){
+    pleaseSolveThisTask("C task");
+}
