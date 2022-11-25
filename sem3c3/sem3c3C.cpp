440f7c248769cfa84cb9c55dfbf0269e8d5f901a
Dimon <konkov.da@phystech.edu>
2022-11-25 22:58:05 +0300

    changed C task

--- sem3c3/sem3c3C.cpp
+++ sem3c3/sem3c3C.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+#include <vector>
 
 const double kEps = 1e-8;
 const int kPrecision = 10;
@@ -9,49 +10,37 @@ template <typename T, typename Q>
 bool IsLess(const T& first, const Q& second) {
   return first + kEps < second;
 }
-template <>
-bool IsLess(const long long& first, const long long& second) {
-  return first < second;
-}
 template <typename T, typename Q>
 bool IsEqual(const T& first, const Q& second) {
   return (first - kEps < second && first + kEps > second);
 }
-template <>
-bool IsEqual(const long long& first, const long long& second) {
-  return first == second;
-}
 template <typename T, typename Q>
 bool IsMore(const T& first, const Q& second) {
   return first - kEps > second;
-}
-template <>
-bool IsMore(const long long& first, const long long& second) {
-  return first > second;
 }
 
 template <typename T = double>
 class Point;
 
 template <typename T>
-double ScalarMultiply(const Point<T>& first, const Point<T>& second);
+T ScalarMultiply(const Point<T>& first, const Point<T>& second);
 template <typename T>
-double VectorMultiply(const Point<T>& first, const Point<T>& second);
+T VectorMultiply(const Point<T>& first, const Point<T>& second);
 
 const double kNoCalculated = -1;
 const double kInfinity = 1e9 + 47;
 
 template <typename T>
 class Point {
- public:
+public:
   Point() : x_(0), y_(0){};
   Point(const T& x, const T& y) : x_(x), y_(y){};
   Point(const Point& other) : x_(other.x_), y_(other.y_){};
   Point(Point&& other) noexcept : x_(other.x_), y_(other.y_){};
   Point(std::initializer_list<T> list)
-      : x_(*list.begin()), y_(*(list.begin() + 1)){};
+          : x_(*list.begin()), y_(*(list.begin() + 1)){};
   Point(const Point& start, const Point& end)
-      : x_(end.x_ - start.x_), y_(end.y_ - start.y_){};
+          : x_(end.x_ - start.x_), y_(end.y_ - start.y_){};
   explicit operator Point<double>() { return Point<double>(x_, y_); }
   Point& operator=(const Point& other) {
     x_ = other.x_;
@@ -123,10 +112,10 @@ class Point {
     return Point(x_ / value, y_ / value);
   }
   double operator/(const Point& other) {
-    if (IsEqual(other.x_, 0.0)) {
-      if (IsEqual(x_, 0.0)) {
-        if (IsEqual(other.y_, 0.0)) {
-          if (IsEqual(y_, 0.0)) {
+    if (IsEqual(other.x_, 0)) {
+      if (IsEqual(x_, 0)) {
+        if (IsEqual(other.y_, 0)) {
+          if (IsEqual(y_, 0)) {
             return 1;
           }
           return kInfinity;
@@ -150,10 +139,10 @@ class Point {
   T& AccessX() { return x_; }
   T& AccessY() { return y_; }
   bool IsCollinearTo(const Point& other) const {
-    return IsEqual(VectorMultiply(*this, other), 0.0);
+    return IsEqual(VectorMultiply(*this, other), 0);
   }
   bool IsPerpendicularTo(const Point& other) const {
-    return IsEqual(ScalarMultiply(*this, other), 0.0);
+    return IsEqual(ScalarMultiply(*this, other), 0);
   }
   void Normalize() {
     Length();
@@ -162,7 +151,7 @@ class Point {
     length_ = 1;
   }
 
- private:
+private:
   T x_;
   T y_;
   mutable double length_ = kNoCalculated;
@@ -175,18 +164,18 @@ Point<T> operator*(const T& value, const Point<T>& point) {
   return Point<T>(point.GetX() * value, point.GetY() * value);
 }
 template <typename T>
-double ScalarMultiply(const Point<T>& first, const Point<T>& second) {
+T ScalarMultiply(const Point<T>& first, const Point<T>& second) {
   return first.GetX() * second.GetX() + first.GetY() * second.GetY();
 }
 template <typename T>
-double VectorMultiply(const Point<T>& first, const Point<T>& second) {
+T VectorMultiply(const Point<T>& first, const Point<T>& second) {
   return first.GetX() * second.GetY() - first.GetY() * second.GetX();
 }
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const Point<T>& point) {
   out << std::setprecision(kPrecision)
-      << (IsEqual(point.GetX(), 0.0) ? 0 : point.GetX()) << " "
-      << (IsEqual(point.GetY(), 0.0) ? 0 : point.GetY());
+      << (IsEqual(point.GetX(), 0) ? 0 : point.GetX()) << " "
+      << (IsEqual(point.GetY(), 0) ? 0 : point.GetY());
   return out;
 }
 template <typename T>
@@ -201,21 +190,21 @@ double Distance(const Line& first, const Line& second);
 double Distance(const Point<double>& point, const Line& line);
 Point<double> GetPointOnLine(double a, double b,
                              double c) {  // line a * x + b * y + c = 0
-  bool is_zero = IsEqual(b, 0.0);
-  return {(is_zero ? -c / a : 0.0), (is_zero ? 0.0 : -c / b)};
+  bool is_zero = IsEqual(b, 0);
+  return {(is_zero ? -c / a : 0), (is_zero ? 0 : -c / b)};
 }
 
 enum class VectorType { Direction, Normal };
 class Line {
- public:
+public:
   // (normal_, direction_) - положительная
   // normal_ в сторону, где > 0
   Line(const double& a, const double& b, const double& c)
-      : direction_(-b, a), normal_(a, b), point_(GetPointOnLine(a, b, c)){};
+          : direction_(-b, a), normal_(a, b), point_(GetPointOnLine(a, b, c)){};
   Line(std::initializer_list<double> list)
-      : Line(*(list.begin()), *(list.begin() + 1), *(list.begin() + 2)){};
+          : Line(*(list.begin()), *(list.begin() + 1), *(list.begin() + 2)){};
   Line(VectorType type, const Point<double>& vector, const Point<double>& point)
-      : point_(point) {
+          : point_(point) {
     if (type == VectorType::Direction) {
       direction_ = vector;
       normal_ = direction_.GetRightNormal();
@@ -226,7 +215,7 @@ class Line {
     }
   }
   Line(const Point<double>& first_point, const Point<double>& second_point)
-      : Line(VectorType::Direction, second_point - first_point, first_point){};
+          : Line(VectorType::Direction, second_point - first_point, first_point){};
   void Normalize() {
     direction_.Normalize();
     normal_.Normalize();
@@ -237,7 +226,7 @@ class Line {
     Point temp_x(normal_.GetX(), other.normal_.GetX());
     Point temp_y(normal_.GetY(), other.normal_.GetY());
     double divider = VectorMultiply(normal_, other.normal_);
-    if (IsEqual(divider, 0.0)) {
+    if (IsEqual(divider, 0)) {
       if (IsEqual(VectorMultiply(point_ - other.point_, direction_), 0)) {
         return point_;
       }
@@ -251,7 +240,7 @@ class Line {
   const Point<double>& GetNormal() const { return normal_; }
   const Point<double>& GetPoint() const { return point_; }
 
- private:
+private:
   Point<double> direction_;
   Point<double> normal_;
   Point<double> point_;
@@ -259,7 +248,7 @@ class Line {
 
 double Distance(const Line& first, const Line& second) {
   if (!first.GetDirection().IsCollinearTo(second.GetDirection())) {
-    return 0.0;
+    return 0;
   }
   return Distance(first.GetPoint(), second);
 }
@@ -273,7 +262,7 @@ double Distance(const Point<T>& first, const Point<T>& second) {
 }
 
 class SegmentsCrossChecker {
- public:
+public:
   bool answer = false;
   Point<double> cross_point = kInfinityPoint;
 
@@ -281,24 +270,24 @@ class SegmentsCrossChecker {
                        const Point<double>& first_end,
                        const Point<double>& second_begin,
                        const Point<double>& second_end)
-      : first_begin_(first_begin),
-        first_end_(first_end),
-        second_begin_(second_begin),
-        second_end_(second_end),
-        first_vector(first_begin, first_end),
-        second_vector(second_begin, second_end),
-        temp_vector(first_begin, second_begin) {
+          : first_begin_(first_begin),
+            first_end_(first_end),
+            second_begin_(second_begin),
+            second_end_(second_end),
+            first_vector_(first_begin, first_end),
+            second_vector_(second_begin, second_end),
+            temp_vector_(first_begin, second_begin) {
     Check();
   };
 
- private:
+private:
   const Point<double>& first_begin_;
   const Point<double>& first_end_;
   const Point<double>& second_begin_;
   const Point<double>& second_end_;
-  Point<double> first_vector;
-  Point<double> second_vector;
-  Point<double> temp_vector;
+  Point<double> first_vector_;
+  Point<double> second_vector_;
+  Point<double> temp_vector_;
 
   void Check() {
     if (first_begin_ == first_end_) {
@@ -317,7 +306,7 @@ class SegmentsCrossChecker {
       IsPointOnSegment(second_begin_, first_begin_, first_end_);
       return;
     }
-    if (temp_vector.IsCollinearTo(first_vector)) {
+    if (temp_vector_.IsCollinearTo(first_vector_)) {
       SegmentsCrossParallel();
       return;
     }
@@ -325,11 +314,11 @@ class SegmentsCrossChecker {
   }
   void IsPointOnSegment(const Point<double>& point, const Point<double>& begin,
                         const Point<double>& end) {
-    temp_vector = Point<double>(begin, point);
+    temp_vector_ = Point<double>(begin, point);
     Point<double> vector(begin, end);
-    if (temp_vector.IsCollinearTo(vector)) {
-      double coef = temp_vector / vector;
-      if (!IsMore(coef, 1.0) && !IsLess(coef, 0.0)) {
+    if (temp_vector_.IsCollinearTo(vector)) {
+      double coef = temp_vector_ / vector;
+      if (!IsMore(coef, 1) && !IsLess(coef, 0)) {
         answer = true;
         cross_point = point;
       }
@@ -337,33 +326,33 @@ class SegmentsCrossChecker {
   }
   void SegmentsCrossParallel() {
     answer = true;
-    double first_coef = temp_vector / first_vector;
-    if (!IsLess(first_coef, 0.0) && !IsMore(first_coef, 1.0)) {
+    double first_coef = temp_vector_ / first_vector_;
+    if (!IsLess(first_coef, 0) && !IsMore(first_coef, 1)) {
       cross_point = second_begin_;
       return;
     }
-    temp_vector = Point<double>(first_begin_, second_end_);
-    double second_coef = temp_vector / first_vector;
-    if (IsMore(first_coef, 1.0) && !IsMore(second_coef, 1.0)) {
+    temp_vector_ = Point<double>(first_begin_, second_end_);
+    double second_coef = temp_vector_ / first_vector_;
+    if (IsMore(first_coef, 1) && !IsMore(second_coef, 1)) {
       cross_point = first_end_;
       return;
     }
-    if (IsLess(first_coef, 0.0) && !IsLess(second_coef, 0.0)) {
+    if (IsLess(first_coef, 0) && !IsLess(second_coef, 0)) {
       cross_point = first_begin_;
       return;
     }
     answer = false;
   }
   void SegmentsCrossNotParallel() {
-    Line first_line(VectorType::Direction, first_vector, first_begin_);
-    Line second_line(VectorType::Direction, second_vector, second_begin_);
+    Line first_line(VectorType::Direction, first_vector_, first_begin_);
+    Line second_line(VectorType::Direction, second_vector_, second_begin_);
     cross_point = first_line.Cross(second_line);
     Point<double> first_cross_vector(first_begin_, cross_point);
     Point<double> second_cross_vector(second_begin_, cross_point);
-    double first_coef = first_cross_vector / first_vector;
-    double second_coef = second_cross_vector / second_vector;
-    if (!IsLess(first_coef, 0.0) && !IsMore(first_coef, 1.0) &&
-        !IsLess(second_coef, 0.0) && !IsMore(second_coef, 1.0)) {
+    double first_coef = first_cross_vector / first_vector_;
+    double second_coef = second_cross_vector / second_vector_;
+    if (!IsLess(first_coef, 0) && !IsMore(first_coef, 1) &&
+        !IsLess(second_coef, 0) && !IsMore(second_coef, 1)) {
       answer = true;
     }
   }
